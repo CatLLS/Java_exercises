56f62ed00f00476773b8ca78cae4054b7f4e9a31
Catarina Silva e Meirelles <catlls622@gmail.com>
2026-02-10 13:54:56 +0000

    aula01

---
+++ Aula01/PrimeiroPrograma.java
@@ -0,0 +1,9 @@
+public class PrimeiroPrograma{
+
+    public static void main(String[] args){/*igual ao C com os args */
+        System.out.println("Olá mundo");/*"System" é uma classe, com uma propriedade "out", que é um vídeo por padrão*/
+        /*o ln pula uma linha para ficar que nem em python, string é sempre em " " */
+        /* o x: no print é o nome do parâmetro que vc dá para a função, é desnecessário, pode tirar */
+    }
+    
+}
---
+++ Aula01/Saudacao.java
@@ -0,0 +1,26 @@
+/*importa tudo aqui em cima! */
+import java.util.Scanner;
+
+
+
+/*criando uma nova classe */
+public class Saudacao{
+    public static void main(String[] args){
+        /*Classes podem ser instanciadas como objetos, importando-as, criando um novo objeto */
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Digite seu nome: ");
+        String nome = entrada.nextLine(); /*tipo, nomen valor; netx line lê tudo até o \n */
+        /*next = ler o próximo string/int/etc, é um método da classe scanner a qual nosso objeto pertence */
+
+        System.out.println("olá, "+nome+"!");/*não pode separar string por vírgula, mas pode concatenar com + */
+
+        System.out.print("Digite sua idade: ");
+        int idade = entrada.nextInt();
+
+        System.out.println("Nome: "+nome+"\nIdade: "+idade);
+        /*ouuu: */
+        System.out.printf("Nome: %s \nIdade: %d\n", nome, idade);
+
+    }
+
+}
