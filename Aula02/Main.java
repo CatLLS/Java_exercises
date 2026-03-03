457fd9d88c614bfade27fdeb54d515eaa781ed82
Catarina Silva e Meirelles <catlls622@gmail.com>
2026-03-03 13:45:41 +0000

    Aula03_ver erros!

--- Aula02/Main.java
+++ Aula02/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Main{
-    public static void main(String, args[]){
+    public static void main(String[] args){
         Contador c1 = new Contador();
         c1.incrementa();
         System.out.printf("C1: %d \n",c1.valor);
